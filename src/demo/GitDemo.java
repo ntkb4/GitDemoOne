b3afe6e9e7719efbde3766874b26974a2495383d
ntkb4 <ntkjasur@gmail.com>
2021-09-30 21:08:54 -0400

    added xyz logic

--- src/demo/GitDemo.java
+++ src/demo/GitDemo.java
@@ -4,6 +4,15 @@ public class GitDemo {
 	
 	public static void main(String[] args) {
 		System.out.println("Git demo....");
+		
+		
+		boolean isSunday = true;
+		
+		if(isSunday) {
+			System.out.println("abc");
+		}else {
+			System.out.println("Xyz");
+		}
 	}
 
 }
